6bea28c7a23579d50884e967bdaa1894efa3d903
max <maxkutovoy@yandex.ru>
2022-03-01 19:46:32 +0600

    Remove unnecessary information

--- short/urls.py
+++ short/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='short_link'),
-    path('<slug:link_slug>/', views.short_url_redirect, name='link_for_redirect')
-    # path('<int:place_id>/', views.about_place)
+    path(
+        '<slug:link_slug>/',
+        views.short_url_redirect,
+        name='link_for_redirect'
+    )
 ]
