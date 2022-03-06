0418c7901ece5938ab8eec9d744225cbf2450856
max <maxkutovoy@yandex.ru>
2022-03-06 13:34:39 +0600

    Separated STATIC_ROOT and STATICFILES_DIR

--- link_shorter/urls.py
+++ link_shorter/urls.py
@@ -19,10 +19,12 @@ from django.contrib import admin
 from django.urls import include
 from django.urls import path
 
-import short
-from short import views
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('short.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
