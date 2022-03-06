753aee2b1a33af3223df7f9ef61a9512e6030c6d
max <maxkutovoy@yandex.ru>
2022-03-06 19:29:06 +0600

    Fix verbose name of slug field in Link model

--- short/models.py
+++ short/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Link(models.Model):
     full_link = models.URLField("Ссылка")
     slug = models.SlugField(
-        "Короткая ссылка",
+        "Slug короткой ссылки",
         max_length=100,
         unique=True,
         db_index=True,
